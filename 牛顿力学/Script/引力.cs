0359294eab8e27faa805b312fc126fdbc364827a
lyaogou <1500608673@qq.com>
2017-04-22 14:00:04 +0800

    shiyan2

---
+++ 牛顿力学/Script/引力.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 引力 : MonoBehaviour {
+		//public Transform planet;
+		private float forceAmountForRotation = 1f;
+		private Vector3 direction;
+		private bool allowForce;
+	   
+
+		void Start()
+		{
+			direction = Vector3.zero;
+		}
+
+		void Update ()
+		{
+
+			allowForce = false;
+
+			if (Input.GetKey(KeyCode.Space))
+				allowForce = true;
+		
+
+		direction = transform.localPosition -Vector3 .zero ;
+			//transform.right = Vector3.Cross(directionOfPlanetFromBird, Vector3.forward);
+		//forward是z轴，cross两个向量的交叉相乘，right向x轴
+
+		}
+	void FixedUpdate ()
+	{
+		if (allowForce)
+		{GetComponent<Rigidbody2D> ().AddForce (-direction * forceAmountForRotation);
+			transform.Translate (-direction*0.1f);}
+	}
+}
---
+++ 牛顿力学/Script/空气阻力.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 空气阻力 : MonoBehaviour {
+
+
+	private bool allowForce;
+	private bool allowForce2;
+	private bool allowForce3;
+	void Start () {
+	}
+	void Update () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		if (rb == null) {
+			return;
+		}
+		allowForce = false;
+		allowForce2 = false;
+		allowForce3 = false;
+
+		if (Input.GetKey(KeyCode.Space))
+			allowForce = true;
+		Vector3 Force=Vector3. zero;
+		Force .y=0.5f;
+		if (allowForce)
+			rb.AddForce (Force);
+
+		if (Input.GetKey(KeyCode.D))
+			allowForce2 = true;
+		Vector3 Force2=Vector3. zero;
+		Force2 .x=0.5f;
+		if (allowForce2)
+			rb.AddForce (Force2);
+		
+		if (Input.GetKey(KeyCode.A))
+			allowForce3 = true;
+		Vector3 Force3=Vector3. zero;
+		Force3 .x=-0.5f;
+		if (allowForce3)
+			rb.AddForce (Force3);
+	}
+
+}
