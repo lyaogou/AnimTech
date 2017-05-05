5e6db83e9104235f10155a5efd7b6781864c751e
lyaogou <1500608673@qq.com>
2017-05-05 13:10:11 +0800

    lizi

---
+++ 粒子系统/Script/34/lizi1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lizi1 : 粒子系统 {
+	
+	//public float _TorquePower = 1.0f;
+	public float _LifeSpan1 = 20.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	public override void Update () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		//rb.velocity *= 0.99;
+
+		base.Update ();
+
+		_LifeSpan1 -= Time.deltaTime;
+	
+
+		Vector3 当前尺寸 = transform.localScale;
+		float x方向当前尺寸 = 当前尺寸.x;
+		float y方向当前尺寸 = 当前尺寸.y;
+
+		float x尺寸变化极限 = x方向当前尺寸 * 0.01f;
+		float y尺寸变化极限 = y方向当前尺寸 * 0.01f;
+
+		float x尺寸随机变化量 = x尺寸变化极限 *_LifeSpan ;
+		float y尺寸随机变化量 = y尺寸变化极限 *_LifeSpan;
+
+		Vector3 随机变化后的尺寸 = new Vector3 (
+			x方向当前尺寸 - x尺寸随机变化量*0.05f,
+			y方向当前尺寸 - y尺寸随机变化量*0.05f);
+		
+		transform.localScale = 随机变化后的尺寸;
+	}
+
+}
---
+++ 粒子系统/Script/34/粒子系统.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class 粒子系统 : MonoBehaviour {
+
+		public float _ForcePower = 1.0f;
+		public float _ForceOffset = 1.0f;
+		public float _LifeSpan = 20.0f;
+
+	private Vector3 direction;
+		// Use this for initialization
+		void Start () {
+		direction = Vector3.zero;
+		}
+
+		// Update is called once per frame
+		public virtual void Update () {
+			LifePassThenDie ();
+			ApplyRandomForce ();
+		}
+
+		void LifePassThenDie ()//随时间消失
+		{
+			_LifeSpan -= Time.deltaTime;
+			if (_LifeSpan < 0.0f) {
+				Destroy (gameObject);
+			}
+		}
+
+		void ApplyRandomForce ()
+		{
+		direction = transform.localPosition*0.1f -Vector3 .zero ;
+			Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+			rb.AddForceAtPosition (_ForcePower * Random.insideUnitCircle, _ForceOffset * Random.insideUnitCircle);//单位圆里的随机点
+		rb.AddForceAtPosition (-direction*5.0f,new Vector2(10,0));
+		//rb.AddForce (_ForcePower * Random.insideUnitCircle );
+
+		}
+}
